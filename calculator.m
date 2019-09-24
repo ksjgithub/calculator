4b23b42d75d2e738a9222e7276773fb6ffe85479
ksjgithub <2227497876@qq.com>
2019-09-24 21:25:04 +0800

    my first commit

---
+++ calculator.m
@@ -0,0 +1,14 @@
+
+function calculator( )
+clear;
+clc;
+close force all;
+
+addpath(genpath('GUI Layout Toolbox 2.3.2\'));
+addpath(genpath('objIDOperatorfunc\'));
+
+mObj = CalculatorModel();
+stdViewObj = StdCalculatorView(mObj);
+registerID('stdViewObj', stdViewObj);
+end
+
